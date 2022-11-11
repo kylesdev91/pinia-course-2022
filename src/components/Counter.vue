636c3966d90b70cf5cddd5de31304556ad0b26f9
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-11-11 17:55:07 -0600

    before state mutations  after need for getters

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>
-        <!-- have count value -->
-        <p>Count Value: {{ store.count }}</p>
-    </div>
+  <div>
+    <br />
+    <button @click="increment">+</button>
+    Count Value: {{ store.count }}
+    <button @click="decrement">-</button>
+    <br>
+    Counter moved to the {{ store.countDigitLength }} digit length
+   
+  </div>
 </template>
 
 <script setup>
-import { useCounterStore } from '../stores/counter';
+import { useCounterStore } from "../stores/counter";
 const store = useCounterStore();
+
+function increment() {
+  store.count++;
+}
+
+function decrement() {
+  store.count--;
+}
+// const countDigitLength = computed(() => store.count.toString().length)
+// const countDigitLength = store.count.toString().length;
 </script>
