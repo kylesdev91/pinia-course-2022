a9e28f62f9f34083b065a3cf42ad508f47e13d54
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-11-11 18:30:00 -0600

    before pinia actions after storeToRefs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 
-import { useAuthStore } from './stores/auth';
+import { useAuthStore } from "./stores/auth";
 const authStore = useAuthStore();
 function logout() {
   // authStore.isAuthenticated = false;
   authStore.$patch((state) => {
-    (state.isAuthenticated = false), (state.user = {})
-   })
+    (state.isAuthenticated = false), (state.user = {});
+  });
 }
 
+function login() {
+  authStore.$reset();
+}
 </script>
 
 <template>
   <header>
-
     <div class="wrapper">
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <button @click="logout">Logout</button>
+        <button @click="logout" v-if="authStore.isAuthenticated">Logout</button>
+        <button @click="login" v-else>Login</button>
       </nav>
     </div>
   </header>
 
-  <RouterView /> 
+  <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup>
+import { storeToRefs } from "pinia";
 import Counter from "../components/Counter.vue";
 import { useAuthStore } from "../stores/auth";
 const authStore = useAuthStore();
+const { isAuthenticated, user } = storeToRefs(authStore)
 </script>
 
 <template>
   <main>
-   <div v-if="authStore.isAuthenticated">Username is: {{ authStore.user.name}}</div>
+   <div v-if="isAuthenticated">Username is: {{ user.name}}</div>
   <Counter />
   </main>
 </template>
