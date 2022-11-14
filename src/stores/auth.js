c7db8a765d2fb4176d63e9cf7d2841c15811d548
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-11-14 07:49:23 -0600

    final commit

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,25 +1,23 @@
 import { defineStore } from "pinia";
+import { computed, reactive, ref } from "vue";
 
-export const useAuthStore = defineStore("auth", {
-  state: () => ({
-    isAuthenticated: false,
-    user: {},
-  }),
-  getters: {
-    fullname: (state) => `${state.user.first_name} ${state.user.last_name}`,
-  },
-  actions: {
-    logout() {
+export const useAuthStore = defineStore("auth", () => {
+  const isAuthenticated = ref(false)
+  const user = reactive({})
+  const fullname = computed(() => `${user.value.first_name} ${user.value.last_name}`)
+
+  function logout() {
       // authStore.isAuthenticated = false;
       this.$patch((state) => {
         (state.isAuthenticated = false), (state.user = {});
       });
-    },
-    async login() {
+    }
+    async function login() {
       const res = await fetch("https://reqres.in/api/users/2?delay=2");
       const { data } = await res.json();
-      this.user = data;
+      this.user.value = data;
       this.isAuthenticated = true;
-    },
-  },
+    }
+    
+    return { login, logout, user, isAuthenticated, fullname}
 });
